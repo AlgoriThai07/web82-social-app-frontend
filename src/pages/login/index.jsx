d7216a551cbf224715b5ebaad0350fdd40ca0862
AlgoriThai07 <thaiviet0703@gmail.com>
2025-03-24 14:02:21 -0500

    add axios

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,78 +1,122 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Button, Form, Input } from 'antd';
-import './styles.scss';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Form, Input, message } from "antd";
+import axios from "axios";
+import "./styles.scss";
 
 const Login = () => {
-    const navigate = useNavigate();
-    const onSubmit = (values) => {
-        console.log('Success:', values);
-    };
-    return (
-        <div className="container-form-login">
-            <h1>Social app MindX Fullstack</h1>
-            <Form
-                onFinish={onSubmit}
-                name="basic"
-                labelCol={{
-                    span: 8,
-                }}
-                wrapperCol={{
-                    span: 16,
-                }}
-                style={{
-                    maxWidth: 600,
-                }}
+  const navigate = useNavigate();
+  const [authenticated, setAuthenticated] = useState({
+    isLoading: null,
+    data: null,
+    message: "",
+    success: false,
+    error: null,
+  });
+  const onSubmit = async (values) => {
+    try {
+      setAuthenticated({
+        ...authenticated,
+        isLoading: true,
+      });
+      const result = await axios.post(
+        "https://web82-social-app-backend-spd5.onrender.com/api/v1/users/sign-in",
+        values
+      );
+      setAuthenticated({
+        ...authenticated,
+        success: true,
+        ...result.data,
+        data: result.data.data,
+        isLoading: false,
+      });
+      console.log("Success:", result);
+    } catch (error) {
+      setAuthenticated({
+        ...authenticated,
+        ...error.response.data,
+        isLoading: false,
+      });
+    }
+  };
+  return (
+    <div className="container-form-login">
+      <h1>Social app MindX Fullstack</h1>
+      <Form
+        onFinish={onSubmit}
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Bạn cần nhập email và đúng định dạng!",
+              type: "email",
+            },
+          ]}
+        >
+          <Input placeholder="example@gmail.com" size="small" />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Bạn cần nhập mật khẩu!",
+            },
+          ]}
+        >
+          <Input.Password size="small" />
+        </Form.Item>
+
+        {authenticated.error && (
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 14,
+            }}
+          >
+            <p style={{ color: "red" }}>{authenticated.message}</p>
+          </Form.Item>
+        )}
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 14,
+          }}
+        >
+          <div className="btn">
+            <Button size="small" htmlType="submit">
+              Đăng nhập
+            </Button>
+            <Button
+              size="small"
+              className="btn-signup"
+              onClick={() => {
+                navigate("/auth/register");
+              }}
             >
-                <Form.Item
-                    label="Email"
-                    name="email"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Bạn cần nhập email và đúng định dạng!',
-                            type: 'email'
-                        },
-                    ]}>
-                    <Input placeholder="example@gmail.com" size="small" />
-                </Form.Item>
+              Đăng ký
+            </Button>
+          </div>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Bạn cần nhập mật khẩu!',
-                        },
-                    ]}
-                >
-                    <Input.Password size="small" />
-                </Form.Item>
-                <Form.Item
-                    wrapperCol={{
-                        offset: 8,
-                        span: 14,
-                    }}
-                >
-                    <div className="btn">
-                        <Button size="small" htmlType="submit">
-                            Đăng nhập
-                        </Button>
-                        <Button
-                            size="small"
-                            className="btn-signup"
-                            onClick={() => {
-                                navigate('/auth/register');
-                            }}
-                        >
-                            Đăng ký
-                        </Button>
-                    </div>
-                </Form.Item>
-            </Form>
-        </div>
-    )
-}
-
-export default Login
+export default Login;
